077512147c6d5eb4fe159c9944aa018ce1b14e3f
Joe Skazinski <joseph.skazinski@seagate.com>
2022-06-22 14:46:18 -0600

    feat: Add 1.4 activate, revoke, locate
    Signed-off-by: Joe Skazinski <joseph.skazinski@seagate.com>

---
+++ op_revoke.go
@@ -0,0 +1,52 @@
+package kmip
+
+import (
+	"context"
+
+	"github.com/Seagate/kmip-go/kmip14"
+)
+
+// 4.20 Revoke
+
+// Table 121
+
+type RevocationReasonStruct struct {
+	RevocationReasonCode kmip14.RevocationReasonCode // Required: Yes
+	RevocationMessage    string                      // Required: No
+}
+
+// Table 212
+
+type RevokeRequestPayload struct {
+	UniqueIdentifier         string                 // Required: No
+	RevocationReason         RevocationReasonStruct // Required: Yes
+	CompromiseOccurrenceDate []byte                 // Required: No
+}
+
+// Table 213
+
+type RevokeResponsePayload struct {
+	UniqueIdentifier string // Required: Yes
+}
+
+type RevokeHandler struct {
+	Revoke func(ctx context.Context, payload *RevokeRequestPayload) (*RevokeResponsePayload, error)
+}
+
+func (h *RevokeHandler) HandleItem(ctx context.Context, req *Request) (*ResponseBatchItem, error) {
+	var payload RevokeRequestPayload
+
+	err := req.DecodePayload(&payload)
+	if err != nil {
+		return nil, err
+	}
+
+	respPayload, err := h.Revoke(ctx, &payload)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ResponseBatchItem{
+		ResponsePayload: respPayload,
+	}, nil
+}
